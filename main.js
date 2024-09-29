8f97774a232564ef15da33c4117208e8b3c20c74
Sundaresh B <sundaresh17@outlook.com>
2024-09-29 20:37:53 +0530

    First Commit

---
+++ main.js
@@ -0,0 +1,12 @@
+function downloadFile(){
+        const link = document.createElement('a');
+        link.href = '\Sundar _testexx.pdf';
+        link.download = 'resume.pdf';
+        link.click();
+}
+function linkedin(){
+        window.open("https://www.linkedin.com/in/sundaresh-bbca1706/, _blank");
+}
+function git(){
+        window.open("https://github.com/stylish-sundar, _blank");
+}
